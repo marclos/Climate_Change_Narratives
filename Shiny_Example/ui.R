1fbd281df01e353036aeca6a803965463594bdd1
marclos <marc.loshuertos@pomona.edu>
2016-07-26 23:40:30 -0700

    added text to shiny example

--- Shiny_Example/ui.R
+++ Shiny_Example/ui.R
@@ -11,12 +11,18 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 
+
+
 shinyUI(fluidPage(
   
   # Application title
   titlePanel("Title"),
   
-
+  tags$h2("Header 2"),
+  
+  tags$code("Welcome to simple Shiny App that we can use to as a template for our Project blog.  Before you spend too much time playing with Shiny, I suggest you look at this", tags$a(href = "http://shiny.rstudio.com/tutorial/", "tutorial")),
+  
+  #tags$hr,
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
