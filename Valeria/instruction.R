049e4dc2a097cbc0b66b5e3aa816d473b0aa4dc2
vesj2015 <vesj2015@mymail.pomona.edu>
2016-12-09 15:51:53 -0800

    Please work 3:49 12/9

--- Valeria/instruction.R
+++ Valeria/instruction.R
@@ -31,31 +31,94 @@ plot(TMAX~NewDate, import, ty='l')
 import.lm <- lm(TMAX~NewDate, data=import)
 summary(import.lm)
 
-#[** doing for individual months? 
-jan.lm = lm(TMAX~NewDate, data=monthlymeans[month[ymeans$month==1]])
-#]
-
-
 
 plot(TMAX~NewDate, import, ty='l')
 abline(coef(import.lm), col='red')
 
 
-# create months and years
+# CREATE VARIABLE MONTHS AND YEARS
+
+import$TMAX_cor <- import$TMAX
+import$TMAX_cor[import$Year<1995 & import$Year>1990] <- NA
+
+
 import$Month = months(import$NewDate)
 
-import$Year = format(import$NewDate, format="%Y")
+import$Year = as.numeric (format(import$NewDate, format="%Y"))
 
 MonthlyMean = aggregate(TMAX ~ Month + Year, import, mean)
-MonthlyMean$YEAR = as.numeric(MonthlyMean$Year)
+
+#dont thinkneed MonthlyMean$YEAR = as.numeric(MonthlyMean$Year)
 head(MonthlyMean)
 tail(MonthlyMean)
 
-YearlyMean = aggregate(TMAX ~ Year, import, mean)
-##MonthlyMean$YEAR = as.numeric(MonthlyMean$Year)
+YearlyMean = aggregate(TMAX_cor ~ Year, import, mean)
+head(YearlyMean)
+##need something like: MonthlyMean$YEAR = as.numeric(MonthlyMean$Year)?
+#probably not
 
-head(MonthlyMean)
-fs
+
+#PLOT THESE AGGREGRATIONS
+#PLOT MONTHLY
+
+plot(MonthlyMean$TMAX, ty='l')
+
+#Plot yearly?
+
+plot(YearlyMean$TMAX, ty='l')
+
+
+#plot SPECIFIC MONTH
+plot(MonthlyMean$TMAX[MonthlyMean$Month=="May"], ty='l')
+plot(TMAX~YEAR, data=MonthlyMean[MonthlyMean$Month=="May",], ty='l') 
+
+
+-----
+#DROP SOME OBSERVATIONS
+
+  #idk why NEed a string but it's here:
+str(import)
+
+#check number of obs (ture and flase =something else)
+sum(import$Year>1990)
+sum(import$Year<1995)
+
+import$TMAX_cor <- import$TMAX
+import$TMAX_cor[import$Year<1995 & import$Year>1990] <- NA
+  
+#just check lenght
+length(import$Year<1995)
+--------
+
+#LINEAR REGRESSIONS
+  
+#Yearly 
+#notdone
+
+  
+  
+Monthly.lm = lm(TMAX_cor ~ MonthlyMean)
+YearlyMean$Year = as.numeric(YearlyMean$Year)
+Yearly.lm = lm(TMAX_cor~ Year, data = YearlyMean)
+summary(Yearly.lm)
+
+plot(TMAX_cor~ Year, data = YearlyMean, ty="l", las=1, ylab="temperature")
+abline(coef(Yearly.lm), col="green")
+#Montly?
+
+May.lm <- lm(TMAX_cor~YEAR, data=MonthlyMean[MonthlyMean$Month=="May",]) 
+summary(May.lm)
+abline(coef(May.lm), col="red")
+
+-----------------------
+#What part of linear regression summary tells you what?
+
+#Coefficients:
+                          #Estimate Std. Error t value Pr(>|t|)    
+# Y-Interc-> #(Intercept) 2.839104   4.078240   0.696 0.487698    
+# SLOPE ->    #Year       0.007178   0.002088   3.438 0.000809 ***
+
+# abline is how u get best fit line
 -----
 #eh i think it ended up not working 
 monthlymeans = aggregate(TMAX[TMAX$ELEMENT=="TMAX",], list(TMAX$YEAR, MONTH), mean)
