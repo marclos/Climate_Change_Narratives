483720eff730f5abb0a778901360196fa6916005
marclos <marc.loshuertos@pomona.edu>
2017-07-06 19:34:28 -0700

    Created Function to convert import csv and convert dates and revised SOP90

---
+++ Example_Code/CSV_import-fun.R
@@ -0,0 +1,11 @@
+# Imports NOAA CSV Files and Prepares them to be analyzed 
+CSV_import <- function(filename=file.choose()){
+  #filename <- file.choose()
+  import = read.csv(filename)
+  import$TMAX[import$TMAX==-9999] = NA
+  import$TMIN[import$TMIN==-9999] = NA
+  strDates <- as.character(import$DATE)
+  #head(strDates)
+  import$NewDate <- as.Date(strDates, "%Y%m%d")
+  return(import)
+}
