06a079d5da05ebefc2caee72f619568ec3df509b
marclos <marc.loshuertos@pomona.edu>
2019-02-10 17:48:16 -0800

    emily data template

--- student_folders/Kuhn/Template.R
+++ student_folders/Kuhn/Template.R
@@ -1,6 +1,11 @@
 ## Read CSV Data
+# file.choose()
 
+filepath = "/home/CAMPUS/mwl04747/github/Climate_Change_Narratives/student_folders/Kuhn/EmilyKuhn_Santabarbara_data.csv"
+climate_data = read.csv(filepath)
 
+str(climate_data)
+plot(TMAX ~ DATE, data=climate_data)
 ## Fix Dates
 
 
