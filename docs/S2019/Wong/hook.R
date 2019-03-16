83dd1b366a7c7616768e57d935d3093b65fefaeb
marclos <marc.loshuertos@pomona.edu>
2019-03-16 16:34:03 -0700

    updated Lai

---
+++ docs/S2019/Wong/hook.R
@@ -0,0 +1,5 @@
+I wrote this blog on in attempt to explore the extreme flooding that occurs in Southern China; however, the
+focus shifted to have a larger focus on the precipitation trends in the Pearl River Basin. This blog belongs
+the attribution science section specifically where it addresses how much we still do not know today about our
+climate, and emphasizes that it is hard to attribute a certain climate pattern to a single extreme event or 
+cause. 
