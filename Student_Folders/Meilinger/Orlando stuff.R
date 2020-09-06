47ee8b0f0d0ea5cf63b30db8131648a531047a35
smac2019 <smac2019@rstudio2.campus.pomona.edu>
2020-09-05 23:20:38 -0700

    9/5/2020 twice over

---
+++ Student_Folders/Meilinger/Orlando stuff.R
@@ -0,0 +1,21 @@
+file.choose(
+  
+)
+read.csv("/home/CAMPUS/smac2019/Climate_Change_Narratives/Sarah_Orlando_Data.csv")
+
+climate_data <- read.csv("/home/CAMPUS/smac2019/Climate_Change_Narratives/Sarah_Orlando_Data.csv")
+head(climate_data)
+str(climate_data)
+names(climate_data)
+## [1] "STATION" "STATION_NAME" "DATE" "PRCP" "TAVG"
+## [6] "TMAX" "TMIN"
+
+plot(TMAX~DATE, climate_data)
+
+min(climate_data$TMAX, na.rm=T)
+## [1] -9999
+
+
+str(climate_data)
+strDates <- as.character(climate_data$DATE)
+climate_data$NewDate <- as.Date(strDates, "%Y-%m-%d")
