248ea09fd10c3defb8089c7027df41687e07cec2
smac2019 <smac2019@rstudio2.campus.pomona.edu>
2020-09-26 20:07:26 -0700

    9/26/20 8:07

--- Student_Folders/Meilinger/Orlando stuff.R
+++ Student_Folders/Meilinger/Orlando stuff.R
@@ -1,11 +1,11 @@
 #file.choose()
 #read.csv("/home/CAMPUS/smac2019/Climate_Change_Narratives/Student_Folders/Meilinger/Sarah_Orlando_Data.csv")
-filepath = "/home/CAMPUS/smac2019/Climate_Change_Narratives/Student_Folders/Meilinger/Sarah_Orlando_Data.csv"
-filepath = "/home/CAMPUS/mwl04747/github/Climate_Change_Narratives/Student_Folders/Meilinger/Sarah_Orlando_Data.csv"
+filepath = "/home/CAMPUS/smac2019/Climate_Change_Narratives/Data/FA20/Sarah_Orlando_Data.csv"
+#filepath = "/home/CAMPUS/mwl04747/github/Climate_Change_Narratives/Student_Folders/Meilinger/Sarah_Orlando_Data.csv"
 
 #climate_data[20000,]
 climate_data_full <- read.csv(filepath)
-climate_data <- subset(climate_data, subset=c(STATION=="USW00012841"))
+climate_data <- subset(climate_data_full, subset=c(STATION=="USW00012841"))
 droplevels(climate_data)
 
 head(climate_data)
@@ -17,10 +17,12 @@ names(climate_data)
 
 plot(TMAX~DATE, climate_data)
 
-min(climate_data$TMAX, na.rm=T)
+min(climate_data$TMAX)
 ## [1] -9999
 
 
 str(climate_data)
 strDates <- as.character(climate_data$DATE)
 climate_data$NewDate <- as.Date(strDates, "%Y-%m-%d")
+
+#hgfdhgdghdhggfdf
