7ca956f9e9c18cb2c4045e51ec478a23d7de63f4
marclos <marc.loshuertos@pomona.edu>
2020-10-15 11:53:28 -0700

    updated lists

--- cp_blogs.R
+++ cp_blogs.R
@@ -6,6 +6,8 @@ Student_Folders <- "Student_Folders"
 docs <- "docs/F2020"
 # find the files that you want
 list.of.folders <- list.files(Student_Folders)
-list.of.files <- list.files(paste(Student_Folders,"/",list.of.folders, sep=""), "*.html")
+list.of.files <- list.files(paste("Student_Folders/",list.of.folders, sep=""), "*.html")
 # copy the files to the new folder
-file.copy(list.of.files, docs)
+Student.html = paste("Student_Folders/",list.of.folders,"/",list.of.folders,".html", sep="")
+file.copy(Student.html, docs, overwrite=TRUE)
+
