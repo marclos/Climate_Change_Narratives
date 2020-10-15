3e67999978efcb878fab2b734bab64f7425db9ba
marclos <marc.loshuertos@pomona.edu>
2020-10-14 19:26:46 -0700

    update Manning

--- cp_blogs.sh
+++ cp_blogs.sh
@@ -6,5 +6,7 @@ echo "Starting Script"
 
 cp Student_Folders/Duggleby/Duggleby.html docs/F2020/Duggleby.html
 cp Student_Folders/Greene/Greene.html docs/F2020/Greene.html
+cp Student_Folders/Manning/Manning.html docs/F2020/Manning.html
+cp Student_Folders/Swiere/Swiere.html docs/F2020/Manning.html
 
 echo "Script Done"
