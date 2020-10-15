12ca391b8dc6e85e49b4b0709b7fb11b94ce2396
marclos <marc.loshuertos@pomona.edu>
2020-10-14 18:32:29 -0700

    cp script

---
+++ cp_blogs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+clear
+
+echo "Starting Script"
+
+cp Student_Folders/Duggleby/Duggleby.html docs/F2020/Duggleby.html
+cp Student_Folders/Greene/Greene.html docs/F2020/Greene.html
+
+echo "Script Done"
